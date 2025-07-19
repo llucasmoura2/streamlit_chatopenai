4948099a3fe1e74afdea3f169dff35dac99e698a
Lucas Moura <154241503+llucasmoura2@users.noreply.github.com>
2025-07-19 00:11:13 -0300

    Delete testes.py
    deletando pasta teste

--- testes.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # carrega as variáveis do arquivo .env para o os.environ
-
-print("Valor de teste_api em os.environ:", os.environ.get("teste_api"))
